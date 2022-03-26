fec4d9f259425b6679f9180718d9acc200c2e58c
Juwon <71490091+wndnjs2037@users.noreply.github.com>
2022-03-26 17:53:59 +0900

    [Bronze II] Title: 숫자의 개수, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,54 +1,8 @@
-# 2577 숫자의 개수
+a = int(input())
+b = int(input())
+c = int(input())
 
-A = int(input())
-B = int(input())
-C = int(input())
+result = list(str(a * b * c)) #리스트에 결과 값을 한 글자씩 쪼개서 넣어주고
 
-result = A * B * C
-result_list = list(map(int, str(result))) #int로 변환할 것이므로 int를 적고, 적용할 값은 str으로 변경하여 넣어준다.
-#print(result_list)
-#print(len(result_list))
-
-count0,count1,count2,count3,count4,count5,count6,count7,count8,count9 = 0,0,0,0,0,0,0,0,0,0
-
-for i in range(0, len(result_list)):
-  if result_list[i] == 0:
-    count0 += 1
-
-  elif result_list[i] == 1:
-    count1 += 1
-  
-  elif result_list[i] == 2:
-    count2 += 1
-
-  elif result_list[i] == 3:
-    count3 += 1
-  
-  elif result_list[i] == 4:
-    count4 += 1
-  
-  elif result_list[i] == 5:
-    count5 += 1
-  
-  elif result_list[i] == 6:
-    count6 += 1
-
-  elif result_list[i] == 7:
-    count7 += 1
-  
-  elif result_list[i] == 8:
-    count8 += 1
-
-  elif result_list[i] == 9:
-    count9 += 1
-
-print(count0)
-print(count1)
-print(count2)
-print(count3)
-print(count4)
-print(count5)
-print(count6)
-print(count7)
-print(count8)
-print(count9)
+for i in range(10): #10번 반복해서(0~9)
+    print(result.count(str(i))) #count 함수를 사용해서 해당 리스트에 문자(0~9)가 몇개씩 있는지 출력
