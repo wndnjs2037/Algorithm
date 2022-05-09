13a36e5afd1515b6b149c837212e52847247a53b
Juwon <71490091+wndnjs2037@users.noreply.github.com>
2022-05-09 09:58:43 +0900

    [Silver V] Title: 단어 정렬, Time: 116 ms, Memory: 35452 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,11 +1,14 @@
-# 1181 단어 정렬
-# sort -> 문자열도 정렬 가능, input()은 느림!
+# 1181 단어정렬 - 개선된 속도로 풀어보기
 
-n = int(input())
+import sys #stdin 을 사용하기 위한 임포트
+
+n = int(sys.stdin.readline())
 words = []
 
 for i in range(n): # 입력받은 만큼 반복
-  words.append(input()) # 입력받은 단어를 words 리스트에 넣어줌
+  words.append(sys.stdin.readline().strip()) 
+  # sys.stdin.readline()은 '\n\'을 포함하는 입력이기 때문에 연속으로 값을 입력받는 for문에서 에러가 발생하므로
+  # strip으로 끊어서 처리해준다.
 
 set_words = set(words) #중복값을 제거하기 위해 set화
 words = list(set_words) #중복값을 제거한 단어들을 다시 list로 전환
